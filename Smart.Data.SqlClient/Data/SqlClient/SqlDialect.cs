89092a90a14803207c7f5203fd98f6e79716b853
usausa <machi.pon@gmail.com>
2017-09-23 20:48:19 +0900

    Add SqlClient project

---
+++ Smart.Data.SqlClient/Data/SqlClient/SqlDialect.cs
@@ -0,0 +1,32 @@
+﻿namespace Smart.Data.SqlClient
+{
+    using System.Data.Common;
+    using System.Data.SqlClient;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public class SqlDialect : IDialect
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public bool IsDuplicate(DbException ex)
+        {
+            return (ex as SqlException)?.Number == 2627;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public string LikeEscape(string value)
+        {
+            return Regex.Replace(value, @"[%_\[]", "[$0]");
+        }
+    }
+}
