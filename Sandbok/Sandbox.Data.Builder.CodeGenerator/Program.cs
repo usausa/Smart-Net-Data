f86112b3359e41c83916faee12b89240c20d6ec8
usausa <machi.pon@gmail.com>
2017-09-23 21:23:35 +0900

    Add sandbox projects

---
+++ Sandbok/Sandbox.Data.Builder.CodeGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sandbox.Data.Builder.CodeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
