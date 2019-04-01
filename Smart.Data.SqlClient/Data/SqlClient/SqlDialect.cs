903dae9f49c0050a9e08a4e6c535069d47c652a5
usausa <machi.pon@gmail.com>
2019-04-01 17:20:58 +0900

    Remove unused comment

--- Smart.Data.SqlClient/Data/SqlClient/SqlDialect.cs
+++ Smart.Data.SqlClient/Data/SqlClient/SqlDialect.cs
@@ -4,26 +4,13 @@
     using System.Data.SqlClient;
     using System.Text.RegularExpressions;
 
-    /// <summary>
-    ///
-    /// </summary>
     public sealed class SqlDialect : IDialect
     {
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="ex"></param>
-        /// <returns></returns>
         public bool IsDuplicate(DbException ex)
         {
             return (ex as SqlException)?.Number == 2627;
         }
 
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
         public string LikeEscape(string value)
         {
             return Regex.Replace(value, @"[%_\[]", "[$0]");
