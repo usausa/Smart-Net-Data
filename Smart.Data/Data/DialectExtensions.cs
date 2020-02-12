41395ba8ec776b508a7232f4279732346bcbd30b
usausa <machi.pon@gmail.com>
2020-02-12 19:28:53 +0900

    Fix extensions

--- Smart.Data/Data/DialectExtensions.cs
+++ Smart.Data/Data/DialectExtensions.cs
@@ -2,21 +2,22 @@ namespace Smart.Data
 {
     using System;
 
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Extensions")]
     public static class DialectExtensions
     {
         public static string LikeStartWith(this IDialect dialect, string value)
         {
-            return String.IsNullOrEmpty(value) ? value : value + "%";
+            return String.IsNullOrEmpty(value) ? value : dialect.LikeEscape(value) + "%";
         }
 
         public static string LikeEndWith(this IDialect dialect, string value)
         {
-            return String.IsNullOrEmpty(value) ? value : "%" + value;
+            return String.IsNullOrEmpty(value) ? value : "%" + dialect.LikeEscape(value);
         }
 
         public static string LikeContains(this IDialect dialect, string value)
         {
-            return String.IsNullOrEmpty(value) ? value : "%" + value + "%";
+            return String.IsNullOrEmpty(value) ? value : "%" + dialect.LikeEscape(value) + "%";
         }
     }
 }
