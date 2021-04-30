01895ddf6b722ec2f4b8a0c8542ceb2643bfe4d7
usausa <machi.pon@gmail.com>
2021-04-30 16:22:24 +0900

    Add dialect

---
+++ Smart.Data/Data/DelegateDialect.cs
@@ -0,0 +1,24 @@
+namespace Smart.Data
+{
+    using System;
+    using System.Data.Common;
+
+    public sealed class DelegateDialect : IDialect
+    {
+        private readonly Func<DbException, bool> isDuplicate;
+
+        private readonly Func<string, string> likeEscape;
+
+        public DelegateDialect(
+            Func<DbException, bool> isDuplicate,
+            Func<string, string> likeEscape)
+        {
+            this.isDuplicate = isDuplicate;
+            this.likeEscape = likeEscape;
+        }
+
+        public bool IsDuplicate(DbException ex) => isDuplicate(ex);
+
+        public string LikeEscape(string value) => likeEscape(value);
+    }
+}
