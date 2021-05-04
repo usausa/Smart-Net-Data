bbaec84f042e3a96158a25f1002dce91cf35af92
usausa <machi.pon@gmail.com>
2021-05-04 10:03:22 +0900

    Fix inspection

--- Smart.Data/Data/DataRecordExtensions.cs
+++ Smart.Data/Data/DataRecordExtensions.cs
@@ -8,42 +8,42 @@ namespace Smart.Data
     {
         public static bool? GetNullableBoolean(this IDataRecord record, int i)
         {
-            return record.IsDBNull(i) ? null : (bool?)record.GetBoolean(i);
+            return record.IsDBNull(i) ? null : record.GetBoolean(i);
         }
 
         public static byte? GetNullableByte(this IDataRecord record, int i)
         {
-            return record.IsDBNull(i) ? null : (byte?)record.GetByte(i);
+            return record.IsDBNull(i) ? null : record.GetByte(i);
         }
 
         public static char? GetNullableChar(this IDataRecord record, int i)
         {
-            return record.IsDBNull(i) ? null : (char?)record.GetChar(i);
+            return record.IsDBNull(i) ? null : record.GetChar(i);
         }
 
         public static DateTime? GetNullableDateTime(this IDataRecord record, int i)
         {
-            return record.IsDBNull(i) ? null : (DateTime?)record.GetDateTime(i);
+            return record.IsDBNull(i) ? null : record.GetDateTime(i);
         }
 
         public static decimal? GetNullableDecimal(this IDataRecord record, int i)
         {
-            return record.IsDBNull(i) ? null : (decimal?)record.GetDecimal(i);
+            return record.IsDBNull(i) ? null : record.GetDecimal(i);
         }
 
         public static double? GetNullableDouble(this IDataRecord record, int i)
         {
-            return record.IsDBNull(i) ? null : (double?)record.GetDouble(i);
+            return record.IsDBNull(i) ? null : record.GetDouble(i);
         }
 
         public static float? GetNullableFloat(this IDataRecord record, int i)
         {
-            return record.IsDBNull(i) ? null : (float?)record.GetFloat(i);
+            return record.IsDBNull(i) ? null : record.GetFloat(i);
         }
 
         public static Guid? GetNullableGuid(this IDataRecord record, int i)
         {
-            return record.IsDBNull(i) ? null : (Guid?)record.GetGuid(i);
+            return record.IsDBNull(i) ? null : record.GetGuid(i);
         }
 
         public static short? GetNullableInt16(this IDataRecord record, int i)
@@ -53,12 +53,12 @@ namespace Smart.Data
 
         public static int? GetNullableInt32(this IDataRecord record, int i)
         {
-            return record.IsDBNull(i) ? null : (int?)record.GetInt32(i);
+            return record.IsDBNull(i) ? null : record.GetInt32(i);
         }
 
         public static long? GetNullableInt64(this IDataRecord record, int i)
         {
-            return record.IsDBNull(i) ? null : (long?)record.GetInt64(i);
+            return record.IsDBNull(i) ? null : record.GetInt64(i);
         }
     }
 }
