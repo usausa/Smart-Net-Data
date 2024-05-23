f2ced6dfb9eadf4fbfd922ee19be68b7cad48ba0
usausa <machi.pon@gmail.com>
2024-05-23 17:52:58 +0900

    Update extensions

--- Smart.Data/Data/DbConnectionExtensions.cs
+++ Smart.Data/Data/DbConnectionExtensions.cs
@@ -2,6 +2,7 @@ namespace Smart.Data;
 
 using System.Data;
 using System.Data.Common;
+using System.Threading;
 using System.Threading.Tasks;
 
 #pragma warning disable CA1062
@@ -15,12 +16,36 @@ public static class DbConnectionExtensions
         }
     }
 
-    public static async ValueTask OpenIfNotAsync(this DbConnection con)
+    public static async ValueTask OpenIfNotAsync(this DbConnection con, CancellationToken cancellationToken = default)
     {
         if ((con.State & ConnectionState.Open) != ConnectionState.Open)
         {
-            await con.OpenAsync().ConfigureAwait(false);
+            await con.OpenAsync(cancellationToken).ConfigureAwait(false);
         }
+    }
+
+    public static DbTransaction BeginTransactionWithOpen(this DbConnection con)
+    {
+        con.OpenIfNot();
+        return con.BeginTransaction();
+    }
+
+    public static DbTransaction BeginTransactionWithOpen(this DbConnection con, IsolationLevel isolationLevel)
+    {
+        con.OpenIfNot();
+        return con.BeginTransaction(isolationLevel);
+    }
+
+    public static async ValueTask<DbTransaction> BeginTransactionWithOpenAsync(this DbConnection con, CancellationToken cancellationToken = default)
+    {
+        await con.OpenIfNotAsync(cancellationToken).ConfigureAwait(false);
+        return await con.BeginTransactionAsync(cancellationToken).ConfigureAwait(false);
+    }
+
+    public static async ValueTask<DbTransaction> BeginTransactionWithOpenAsync(this DbConnection con, IsolationLevel isolationLevel, CancellationToken cancellationToken = default)
+    {
+        await con.OpenIfNotAsync(cancellationToken).ConfigureAwait(false);
+        return await con.BeginTransactionAsync(isolationLevel, cancellationToken).ConfigureAwait(false);
     }
 }
 #pragma warning restore CA1062
