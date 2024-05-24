530cee18b3db47bfe0b5c74195edad20e07fb102
usausa <machi.pon@gmail.com>
2024-05-24 09:50:10 +0900

    Update extensions

--- Smart.Data/Data/DbProviderExtensions.cs
+++ Smart.Data/Data/DbProviderExtensions.cs
@@ -268,5 +268,151 @@ public static class DbProviderExtensions
 #pragma warning restore CA2007
         return await func(con, tx, state).ConfigureAwait(false);
     }
+
+    public static void UsingAutoTx(this IDbProvider factory, Action<DbConnection, DbTransaction> action)
+    {
+        using var con = factory.CreateConnection();
+        con.Open();
+        using var tx = con.BeginTransaction();
+        action(con, tx);
+        tx.Commit();
+    }
+
+    public static void UsingAutoTx<TState>(this IDbProvider factory, TState state, Action<DbConnection, DbTransaction, TState> action)
+    {
+        using var con = factory.CreateConnection();
+        con.Open();
+        using var tx = con.BeginTransaction();
+        action(con, tx, state);
+        tx.Commit();
+    }
+
+    public static TResult UsingAutoTx<TResult>(this IDbProvider factory, Func<DbConnection, DbTransaction, TResult> func)
+    {
+        using var con = factory.CreateConnection();
+        con.Open();
+        using var tx = con.BeginTransaction();
+        var result = func(con, tx);
+        tx.Commit();
+        return result;
+    }
+
+    public static TResult UsingAutoTx<TResult, TState>(this IDbProvider factory, TState state, Func<DbConnection, DbTransaction, TState, TResult> func)
+    {
+        using var con = factory.CreateConnection();
+        con.Open();
+        using var tx = con.BeginTransaction();
+        var result = func(con, tx, state);
+        tx.Commit();
+        return result;
+    }
+
+    public static async ValueTask UsingAutoTxAsync(this IDbProvider factory, Func<DbConnection, DbTransaction, ValueTask> func)
+    {
+#pragma warning disable CA2007
+        await using var con = factory.CreateConnection();
+#pragma warning restore CA2007
+        await con.OpenAsync().ConfigureAwait(false);
+#pragma warning disable CA2007
+        await using var tx = await con.BeginTransactionAsync().ConfigureAwait(false);
+#pragma warning restore CA2007
+        await func(con, tx).ConfigureAwait(false);
+        await tx.CommitAsync().ConfigureAwait(false);
+    }
+
+    public static async ValueTask UsingAutoTxAsync<TState>(this IDbProvider factory, TState state, Func<DbConnection, DbTransaction, TState, ValueTask> func)
+    {
+#pragma warning disable CA2007
+        await using var con = factory.CreateConnection();
+#pragma warning restore CA2007
+        await con.OpenAsync().ConfigureAwait(false);
+#pragma warning disable CA2007
+        await using var tx = await con.BeginTransactionAsync().ConfigureAwait(false);
+#pragma warning restore CA2007
+        await func(con, tx, state).ConfigureAwait(false);
+        await tx.CommitAsync().ConfigureAwait(false);
+    }
+
+    public static async ValueTask<TResult> UsingAutoTxAsync<TResult>(this IDbProvider factory, Func<DbConnection, DbTransaction, ValueTask<TResult>> func)
+    {
+#pragma warning disable CA2007
+        await using var con = factory.CreateConnection();
+#pragma warning restore CA2007
+        await con.OpenAsync().ConfigureAwait(false);
+#pragma warning disable CA2007
+        await using var tx = await con.BeginTransactionAsync().ConfigureAwait(false);
+#pragma warning restore CA2007
+        var result = await func(con, tx).ConfigureAwait(false);
+        await tx.CommitAsync().ConfigureAwait(false);
+        return result;
+    }
+
+    public static async ValueTask<TResult> UsingAutoTxAsync<TResult, TState>(this IDbProvider factory, TState state, Func<DbConnection, DbTransaction, TState, ValueTask<TResult>> func)
+    {
+#pragma warning disable CA2007
+        await using var con = factory.CreateConnection();
+#pragma warning restore CA2007
+        await con.OpenAsync().ConfigureAwait(false);
+#pragma warning disable CA2007
+        await using var tx = await con.BeginTransactionAsync().ConfigureAwait(false);
+#pragma warning restore CA2007
+        var result = await func(con, tx, state).ConfigureAwait(false);
+        await tx.CommitAsync().ConfigureAwait(false);
+        return result;
+    }
+
+    public static async ValueTask UsingAutoTxAsync(this IDbProvider factory, IsolationLevel level, Func<DbConnection, DbTransaction, ValueTask> func)
+    {
+#pragma warning disable CA2007
+        await using var con = factory.CreateConnection();
+#pragma warning restore CA2007
+        await con.OpenAsync().ConfigureAwait(false);
+#pragma warning disable CA2007
+        await using var tx = await con.BeginTransactionAsync(level).ConfigureAwait(false);
+#pragma warning restore CA2007
+        await func(con, tx).ConfigureAwait(false);
+        await tx.CommitAsync().ConfigureAwait(false);
+    }
+
+    public static async ValueTask UsingAutoTxAsync<TState>(this IDbProvider factory, IsolationLevel level, TState state, Func<DbConnection, DbTransaction, TState, ValueTask> func)
+    {
+#pragma warning disable CA2007
+        await using var con = factory.CreateConnection();
+#pragma warning restore CA2007
+        await con.OpenAsync().ConfigureAwait(false);
+#pragma warning disable CA2007
+        await using var tx = await con.BeginTransactionAsync(level).ConfigureAwait(false);
+#pragma warning restore CA2007
+        await func(con, tx, state).ConfigureAwait(false);
+        await tx.CommitAsync().ConfigureAwait(false);
+    }
+
+    public static async ValueTask<TResult> UsingAutoTxAsync<TResult>(this IDbProvider factory, IsolationLevel level, Func<DbConnection, DbTransaction, ValueTask<TResult>> func)
+    {
+#pragma warning disable CA2007
+        await using var con = factory.CreateConnection();
+#pragma warning restore CA2007
+        await con.OpenAsync().ConfigureAwait(false);
+#pragma warning disable CA2007
+        await using var tx = await con.BeginTransactionAsync(level).ConfigureAwait(false);
+#pragma warning restore CA2007
+        var result = await func(con, tx).ConfigureAwait(false);
+        await tx.CommitAsync().ConfigureAwait(false);
+        return result;
+    }
+
+    public static async ValueTask<TResult> UsingAutoTxAsync<TResult, TState>(this IDbProvider factory, IsolationLevel level, TState state, Func<DbConnection, DbTransaction, TState, ValueTask<TResult>> func)
+    {
+#pragma warning disable CA2007
+        await using var con = factory.CreateConnection();
+#pragma warning restore CA2007
+        await con.OpenAsync().ConfigureAwait(false);
+#pragma warning disable CA2007
+        await using var tx = await con.BeginTransactionAsync(level).ConfigureAwait(false);
+#pragma warning restore CA2007
+        var result = await func(con, tx, state).ConfigureAwait(false);
+        await tx.CommitAsync().ConfigureAwait(false);
+        return result;
+    }
 }
 #pragma warning restore CA1062
