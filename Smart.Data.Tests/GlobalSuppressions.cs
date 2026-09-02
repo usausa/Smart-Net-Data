43f38c9b4adb4756fd356d0f7d304c04dc7e43c7
usausa <machi.pon@gmail.com>
2026-09-02 16:21:46 +0900

    Update

--- Smart.Data.Tests/GlobalSuppressions.cs
+++ Smart.Data.Tests/GlobalSuppressions.cs
@@ -1,1 +1,2 @@
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1034:NestedTypesShouldNotBeVisible", Justification = "Ignore")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Do not directly await a Task", Justification = "Ignore")]
